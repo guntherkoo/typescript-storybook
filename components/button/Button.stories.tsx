73abd2fcae05cb937c7b5b5d6599f44c6d348c0e
Gunther Koo <gunther.koo@gmail.com>
2021-01-16 16:38:46 -0500

    add scss declaration, update type states for button component

--- components/button/Button.stories.tsx
+++ components/button/Button.stories.tsx
@@ -1,12 +1,31 @@
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
-import Button from './Button';
+import { withKnobs, text, select, color } from '@storybook/addon-knobs';
+
+import Button, {ColorStates, VariantStates} from './Button';
 
 storiesOf('Button', module)
+	.addDecorator(withKnobs({
+		escapeHTML: false,
+	}))
 	.addDecorator(centered)
-	.add('with text', () => {
-		return <Button text='Hello World' />;
+	.add('contained', () => {
+		return (
+			<Button
+				color={select('color', ColorStates, ColorStates.DARK_NAVY)}
+				text={text('text', 'MainStreet')}
+				variant={select('variant', VariantStates, VariantStates.CONTAINED)}
+				ariaLabel={text('aria-label', 'Button')}
+			/>
+		);
 	})
-	.add('with emoji', () => {
-		return <Button text='😀 😎 👍 💯' />;
+	.add('outlined', () => {
+		return (
+			<Button
+				color={select('color', ColorStates, ColorStates.ROYAL_BLUE)}
+				text={text('text', 'MainStreet')}
+				variant={select('variant', VariantStates, VariantStates.OUTLINED)}
+				ariaLabel={text('aria-label', 'Button')}
+			/>
+		);
 	});
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,21 +1,51 @@
-import * as React from 'react';
+import React from 'react';
 import classnames from 'classnames/bind';
 
 import _styles from './Button.scss';
 
 const styles = classnames.bind(_styles);
 
-type Props = {
-	text: string;
+
+export enum ColorStates {
+	ROYAL_BLUE = 'royal-blue',
+	DARK_NAVY = 'dark-navy',
+	EMERALD = 'emerald',
 };
 
-export default ({ text }: Props) => {
+export enum VariantStates {
+	OUTLINED = 'outlined',
+	CONTAINED = 'contained',
+};
+
+type ButtonProps = {
+	className?: string,
+	color: ColorStates,
+	text: string,
+	ariaLabel: string,
+	variant: VariantStates,
+};
+
+const Button: React.FC<ButtonProps> = ({
+	className,
+	color,
+	text,
+	ariaLabel,
+	variant,
+}) => {
+	const composed_classname = classnames(
+		styles('btn', color, variant),
+		className,
+	);
+
 	return (
 		<button
-			className={styles('btn')}
 			type='button'
+			className={composed_classname}
+			aria-label={ariaLabel}
 		>
 			{text}
 		</button>
 	);
 };
+
+export default Button;

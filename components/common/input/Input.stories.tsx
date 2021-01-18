6308e418fd048471a1fa54dd8252224e8c9c4c79
Gunther Koo <gunther.koo@gmail.com>
2021-01-17 22:16:18 -0500

    added input component

---
+++ components/common/input/Input.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import { withKnobs, text, select} from '@storybook/addon-knobs';
+
+import Input, {
+	TypeState
+} from './Input';
+
+storiesOf('common/input', module)
+	.addDecorator(withKnobs({
+		escapeHTML: false,
+	}))
+	.addDecorator(centered)
+	.add('default', () => {
+		const [value, setValue] = useState('');
+
+		const onChange = e => {
+			const new_value = e.target.value;
+			setValue(new_value);
+		}
+
+		return (
+			<Input
+				name={text('name', 'company')}
+				type={select('type', TypeState, TypeState.TEXT)}
+				label={text('label', 'Company')}
+				value={value}
+				onChange={e => onChange(e)}
+			/>
+		);
+	});
---
+++ components/common/input/Input.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import classnames from 'classnames/bind';
+
+import _styles from './Input.scss';
+
+const styles = classnames.bind(_styles);
+
+export enum TypeState {
+	TEXT = 'text',
+	NUMBER = 'number',
+	PASSWORD = 'password',
+};
+
+interface InputProps {
+	className?: string;
+	name: string;
+	type: string;
+	label: string;
+	value: string;
+	onChange: (value: string) => void;
+};
+
+const Input: React.FC<InputProps> = ({
+	className,
+	name,
+	type,
+	label,
+	value,
+	onChange,
+}) => {
+	const [onFocus, setOnFocus] = useState(false);
+	const [isFilled, setIsFilled] = useState(false);
+
+	useEffect(() => {
+		if (value.length > 0) {
+			setIsFilled(true);
+		} else {
+			setIsFilled(false);
+		}
+	}, [onFocus]);
+
+	return (
+		<div
+			className={styles('container', {
+				'focused': onFocus,
+				'filled': isFilled,
+			})}
+		>
+			<label
+				htmlFor={name}
+			>
+				{label}
+			</label>
+			<input
+				id={name}
+				name={name}
+				type={type}
+				placeholder={label}
+				value={value}
+				onFocus={() => setOnFocus(true)}
+				onBlur={() => setOnFocus(false)}
+				onChange={e => onChange(e)}
+			/>
+		</div>
+	)
+};
+
+export default Input;
